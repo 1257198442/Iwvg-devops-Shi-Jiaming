96f6b603b840aaf475bcbffed890fb27a1e6b4ec
1257198442 <1257198442@qq.com>
2022-10-05 23:15:27 +0200

    added testFindUserIdBySomeProperFraction in SearchesTest #16

--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -1,5 +1,9 @@
 package es.upm.miw.iwvg_devops.code;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -7,5 +11,9 @@ public class SearchesTest {
     @Test
     void testFindFirstDecimalFractionByUserName() {
         assertEquals(0.20, new Searches().findFirstDecimalFractionByUserName("Oscar"));
+    }
+    @Test
+    void testFindUserIdBySomeProperFraction(){
+        assertEquals(Arrays.asList(new String[]{"1","2","3","5"}),new Searches().findUserIdBySomeProperFraction(1).collect(Collectors.toList()));
     }
 }
